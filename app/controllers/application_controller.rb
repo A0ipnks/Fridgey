619baa9804092695f909d45f9b6a2d697b93dfbc
A.0.!.p.n.k.s <n.blue10222201@gmail.com>
2025-08-04 11:15:00 +0900

    アプリケーションコントローラに認証機能とパラメータ設定を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,17 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   private
 
   def after_sign_in_path_for(resource)
     rooms_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:display_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:display_name])
   end
 end
